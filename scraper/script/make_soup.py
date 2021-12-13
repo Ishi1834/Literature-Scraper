8e00021ebcb5d93afe3db308cdb3d0ef8cad3f9c
Ismail Mohamad <ikmohamad19@gmail.com>
2021-12-13 16:26:54 +0000

    Created a test for make_soup module using requests-mock library

--- scraper/script/make_soup.py
+++ scraper/script/make_soup.py
@@ -2,20 +2,16 @@ import requests
 import random 
 from bs4 import BeautifulSoup
 import time
-from script.create_headers import process_headers
+from script.create_headers import process_headers 
 
 headers_list = process_headers()
 def create_soup(link):
     #Pick a random browser headers
     headers = random.choice(headers_list)
-    #Create a request session
-    r = requests.Session()
-    r.headers = headers
-    response = r.get(link)
-    time.sleep(5) # will sleep 5 sec after each request so the site doesn't respond with error 403
-    print(str(response))
-    
-    
+    response = requests.get(link, headers) 
+    #will sleep 5 sec after each request so the site doesn't respond with error 403
+    time.sleep(5)
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
+
     return soup
